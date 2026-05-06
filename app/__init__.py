42c32f321143bdbc5d848d8da325fe7eca305678
Orville Daley <orvilledaley2004@gmail.com>
2026-05-06 18:57:56 -0500

    finishing up backend

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+from flask_login import LoginManager
 
 from .config import Config
 
@@ -13,9 +14,7 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-
-
+login_manager = LoginManager(app)
 
 # Allow Vue frontend to communicate with Flask backend
 CORS(app, supports_credentials=True)
@@ -24,6 +23,8 @@ CORS(app, supports_credentials=True)
 os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 # Import models and register blueprints
+# Models expect `login_manager` to be available from the `app` package,
+# so create/init the manager above before importing models.
 from app import models
 from app.routes import register_blueprints
 
